34ad92952d86ce5c1bb01f462f2e8361dfc14e0f
urgrund <mattbthe1st@gmail.com>
2019-10-31 16:21:07 +0800

    added more images for VL and fixed up style

--- jsscript.js
+++ jsscript.js
@@ -1,7 +1,6 @@
 
 
-function image(url, divid, subtext, contid, imgid)
-{
+function image(url, divid, subtext, contid, imgid) {
 	//if (contid == undefined)
 	contid = "sscontainer";
 	//if (imgid == undefined)
@@ -38,41 +37,42 @@ function image(url, divid, subtext, contid, imgid)
 
 	// The modal image popup
 	var b = document.createElement("div");
-
-	b.innerHTML = "<img class='modal-content' src='" + url + "'/>";
+	var innerHTML = "";
+	innerHTML += "<img class='modal-content' src='" + url + "'/>";
+	innerHTML += "<br/>";
+	innerHTML += "<h3 class='center'>" + subtext + "</h3>";
+	b.innerHTML = innerHTML;
 	b.className = 'modal';
 	b.id = url;
 
 	// add the two image links to teh ss div
-	ssdiv.appendChild(a);
+	//ssdiv.appendChild(a);
 	//ssdiv.appendChild(b);
 	document.getElementById("modalbox").appendChild(b);
 
 	// the clicks to open/close
-	imgDiv.onclick = function ()
-	{
+	imgDiv.onclick = function () {
 		console.log("alksdjlks");
 		b.style.display = "block";
 	}
 
 	// When the user clicks on <span> (x), close the modal
-	b.onclick = function ()
-	{
+	b.onclick = function () {
 		b.style.display = "none";
 	}
 
 
 	// add the ss div to the new div
-	newdiv.appendChild(ssdiv);
+	newdiv.appendChild(a);
 
 
 	// Add the text if available	
-	if (subtext != undefined)
-	{
-		var txtd = document.createElement("div");
-		txtd.innerHTML += "<p>" + subtext + "</p>";
-		newdiv.appendChild(txtd);
-	}
+	// if (subtext != undefined)
+	// {
+	// 	var txtd = document.createElement("div");
+	// 	txtd.innerHTML += "<p>" + subtext + "</p>";
+	// 	newdiv.appendChild(txtd);
+	// }
 
 	document.getElementById(divid).appendChild(newdiv);
 }
