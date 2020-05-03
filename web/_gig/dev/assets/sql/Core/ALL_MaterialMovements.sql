4e29cdb7a4b168bf3de8982a4067b3314dc20e65
urgrund <mattbthe1st@gmail.com>
2020-05-03 19:32:03 +0800

    [gig] sql core updated0

--- web/_gig/dev/assets/sql/Core/ALL_MaterialMovements.sql
+++ web/_gig/dev/assets/sql/Core/ALL_MaterialMovements.sql
@@ -1,5 +1,6 @@
 SELECT 
 	RDP.[Source_MineArea_Code] as 'Source Mine'
+	,substring(ats.ShKey,10,1) as 'Shift'
 	,RDP.[Source_Category] as 'Category'
     ,RDP.[Source_Description_Code] as 'Source Area'
 	,RDP.[Source_Description] as 'Source Labels'
@@ -8,8 +9,8 @@ SELECT
 	,RDD.[Destination_MineArea_Code] as 'Destination Mine'
 	,SUM(MTS.[MeasureValue]) AS 'Tonnes'
 	,count(MTS.[MeasureValue]) AS 'Count'
-	,case when RDP.[Source_Category] = 'Stockpile' then '1' else '0' end as 'Start position'
-	,case when RDD.[Destination_Description] in ('U/G Crusher__O', 'M35 Surface Ore Stockpile', 'M35 Surface Waste Stockpile', 'M35 Surface Stope Stockpile') then '3' else case when RDP.[Source_Category] = 'Stockpile' then '2' else '1' end end as 'Finish position'
+	,case when RDP.[Source_Category] = 'Stockpile' then 1 else 0 end as 'Start position'
+	,case when RDD.[Destination_Description] in ('U/G Crusher__O', 'M35 Surface Ore Stockpile', 'M35 Surface Waste Stockpile', 'M35 Surface Stope Stockpile') then 3 else case when RDP.[Source_Category] = 'Stockpile' then 2 else 1 end end as 'Finish position'
 	FROM [dbo].[ALLOCTNTIMESTAMP]		as ATS 
 	Join [dbo].[MEASURETIMESTAMP]		as MTS on MTS.[TSKey] = ATS.[TSKey]
     Join [dbo].[RD_EQUIPMENT]			as RDE on RDE.[EquipmentCode] = ATS.[Equipment]
@@ -18,9 +19,9 @@ SELECT
 	Join [dbo].[RD_EQUIPMENT_MODEL]		as RDM on RDM.[Equipment_ModelCode] = RDE.[Equipment_Model_Code]
 	Join [dbo].[RD_EQUIPMENT_FUNCTION]	as RDF on RDF.[Equipment_FunctionCode] = RDM.[Equipment_Model_Function_Code]
 	
-	WHERE ATS.ShKey like '20181001P%' 
+	WHERE ATS.ShKey = @Date 
 		--and left(RDP.[Source_MineArea_Code],3) Like 'SLC'
 		and RDP.[Source_MineArea_Code] <> 'MISC' 
 		and MTS.[MeasCode] = 'TONNE' 
 		and RDF.[Equipment_FunctionCode] = 'Loading'
-	GROUP BY RDP.[Source_Category], RDP.[Source_MineArea_Code], RDP.[Source_Description_Code],RDP.[Source_Description], RDD.[Destination_Description_Code],RDD.[Destination_Description], RDD.[Destination_MineArea_Code] 
+	GROUP BY RDP.[Source_Category], ats.shkey, RDP.[Source_MineArea_Code], RDP.[Source_Description_Code],RDP.[Source_Description], RDD.[Destination_Description_Code],RDD.[Destination_Description], RDD.[Destination_MineArea_Code] 
